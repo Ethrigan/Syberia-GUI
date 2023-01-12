030452301a1ccc1d1c4ca521b22ff18d5b19c031
Ethrigan Daemonicus <ethrigan.daemonicus@gmail.com>
2023-01-12 01:40:02 -0300

    Frames e imagem

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import *
+from PIL import ImageTk, Image
 
 # root window
 root = Tk()
@@ -16,6 +17,7 @@ styl.configure('red.TSeparator', background='red')
 styl.configure('blk.TSeparator', background='black')
 
 
+# funcoes
 def focus(e, nome):
     if nome == "lab1":
         lab1["foreground"] = "red"
@@ -34,30 +36,77 @@ def unfocus(e, nome):
         lab3["foreground"] = "black"
 
 
-lab1 = Label(root, text="Doenças", font="Helvetica 10 bold", justify="left", cursor="hand1", fg="black")
+def doencas_class(event):
+    for widget in frame2.winfo_children():
+        widget.destroy()
+
+    ldoencas = Label(frame2, text="doencas")
+    ldoencas.pack()
+
+
+def medicamentos_class(event):
+    for widget in frame2.winfo_children():
+        widget.destroy()
+
+    lmeds = Label(frame2, text="medicamentos")
+    lmeds.pack()
+
+
+def legendas_class(event):
+    for widget in frame2.winfo_children():
+        widget.destroy()
+
+    llegendas = Label(frame2, image=imagem)
+    llegendas.pack()
+
+
+# widgets
+frame1 = Frame(root, borderwidth=0, width=0, height=100, relief=SOLID)
+frame2 = Frame(root, borderwidth=0, width=0, height=0, relief=SOLID, padx=5, pady=5)
+#frame3 = Frame(root, bg='#FF0000', borderwidth=1, width=0, height=50, relief=SOLID)
+
+lab1 = Label(frame1, text="Doenças", font="Helvetica 10 bold", justify="left", cursor="hand1", fg="black", padx=10,
+             pady=10)
 lab1.bind("<Enter>", lambda e: focus(e, "lab1"))
 lab1.bind("<Leave>", lambda e: unfocus(e, "lab1"))
 
-lab2 = Label(root, text="Medicamentos", font="Helvetica 10 bold", justify="left", cursor="hand1", fg="black")
+lab2 = Label(frame1, text="Medicamentos", font="Helvetica 10 bold", justify="left", cursor="hand1", fg="black", padx=10,
+             pady=10)
 lab2.bind("<Enter>", lambda e: focus(e, "lab2"))
 lab2.bind("<Leave>", lambda e: unfocus(e, "lab2"))
 
-lab3 = Label(root, text="Legendas", font="Helvetica 10 bold", justify="left", cursor="hand1", fg="black")
+lab3 = Label(frame1, text="Legendas", font="Helvetica 10 bold", justify="left", cursor="hand1", fg="black", padx=10,
+             pady=10)
 lab3.bind("<Enter>", lambda e: focus(e, "lab3"))
 lab3.bind("<Leave>", lambda e: unfocus(e, "lab3"))
 
-lab1.grid(row=0, column=0, padx=10, sticky=(E, W))
-lab2.grid(row=0, column=1, padx=10, sticky=(E, W))
-lab3.grid(row=0, column=2, padx=10, sticky=(E, W))
+lab_a = lab1.pack(side=LEFT)
+lab_b = lab2.pack(side=LEFT)
+lab_c = lab3.pack(side=LEFT)
 
 # horizontal separator
-ttk.Separator(
-    master=root,
+separador_a = ttk.Separator(
+    master=frame1,
     orient=HORIZONTAL,
     style='blk.TSeparator',
     class_=ttk.Separator,
     takefocus=1,
     cursor=''
-).grid(row=2, column=0, ipadx=800, pady=0, columnspan=99, sticky=(W, E))
+).pack
+
+frame1.pack(side="top", fill="x")
+frame2.pack(expand=TRUE, fill=BOTH)
+#frame3.pack(side="bottom", fill="x")
+
+# binds
+lab1.bind("<Button-1>", doencas_class)
+lab2.bind("<Button-1>", medicamentos_class)
+lab3.bind("<Button-1>", legendas_class)
+
+# Create an object of tkinter ImageTk
+imagem = Image.open("imagens/legendas.png")
+imagem = imagem.resize((640,480))
+imagem = ImageTk.PhotoImage(imagem)
+#
 
 root.mainloop()
